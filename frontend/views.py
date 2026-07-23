325bf4270dd121832b6384418396b4a69fdb1ebc
John Kingoo <kingoojohn27@gmail.com>
2026-07-23 17:10:01 +0300

    Connect to the Backend

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,19 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+API_URL = "http://127.0.0.1:8000/api/students/"
+
+
+def students(request):
+    response = requests.get(API_URL)
+
+    if response.status_code == 200:
+        students = response.json()
+    else:
+        students = []
+
+    return render(
+        request,
+        "frontend/students.html",
+        {"students": students},
+    )
