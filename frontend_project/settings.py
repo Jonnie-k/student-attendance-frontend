887ce62c06740d1a3f50c99b083d09360439cf70
John Kingoo <kingoojohn27@gmail.com>
2026-07-27 06:57:44 +0300

    Fix static files path for CSS

--- frontend_project/settings.py
+++ frontend_project/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR /"frontend_project" /"static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = (
